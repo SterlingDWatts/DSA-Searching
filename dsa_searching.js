6fb77a2f706c74c62e8fcd524899068a7e656ef7
Sterling Watts <SterlingDWatts@gmail.com>
2020-04-15 16:46:33 -0700

    Second Commit

--- dsa_searching.js
+++ dsa_searching.js
@@ -1,3 +1,17 @@
+const BinarySearchTree = require("./binarySearchTree");
+
+/*
+4. Searching in a BST
+  1) postorder traversal:
+    12 9 15 27 25 79 90 91 89 35
+  2) pre-order traversal:
+    8 6 5 7 10 9 11
+
+6. Find the next commanding officer
+  Create a linked list of the tree using a breadth first search
+  When somebody dies, remove them from the list and rebuild the tree
+*/
+
 const dsaSearching = {
   main() {
     const arr = [3, 5, 6, 11, 12, 14, 15, 17, 18];
@@ -23,6 +37,59 @@ const dsaSearching = {
     } else if (item > value) {
       return this.binarySearch(array, value, start, index - 1);
     }
+  },
+  deweySearch(bookArray, deweyNum, book, index) {
+    var index = index === undefined ? this.binarySearch(bookArray, deweyNum) : index
+
+    if (book == bookArray[index]) {
+      return bookArray[index]
+    } else if (book < bookArray[index]) {
+      return this.deweySearch(bookArray, deweyNum, book, index - 1)
+    } else {
+      return this.deweySearch(bookArray, deweyNum, book, index + 1)
+    }
+  },
+  inOrder(tree, values = []) {
+    if (tree.left) {
+      values = this.inOrder(tree.left, values)
+    }
+    values.push(tree.value);
+    if (tree.right) {
+      values = this.inOrder(tree.right, values)
+    }
+    return values;
+  },
+  preOrder(tree, values = []) {
+    values.push(tree.value);
+    if (this.left) {
+      values = this.preOrder(tree.left, values);
+    } 
+    if (this.right) {
+      values = this.preOrder(tree.right, values);
+    }
+    return values;
+  },
+  postOrder(tree, values = []) {
+    if (this.left) {
+      values = this.postOrder(tree.left, values)
+    }
+    if (this.right) {
+      values = this.postOrder(tree.right, values)
+    }
+    values.push(tree.value)
+    return values;
+  },
+  maxProfit(arr) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        const profit = arr[j] - arr[i];
+        if (profit > max) {
+          max = profit;
+        }
+      }
+    }
+    return max;
   }
 }
 
--- reactSearch/react-search/src/App.js
+++ reactSearch/react-search/src/App.js
@@ -1,11 +1,86 @@
 import React, { Component } from 'react';
+import numberArray from "./number_array";
 import './App.css';
 
 class App extends Component {
+
+  linearSearch = (array, value) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  binarySearch = (array, value, sortedArray, start, end, numTries) => {
+    var sortedArray = sortedArray === undefined ? array.sort() : sortedArray;
+    var start = start === undefined ? 0 : start;
+    var end = end === undefined ? array.length : end;
+    var numTries = numTries === undefined ? 1 : numTries;
+
+    if (start > end) {
+      return [-1, numTries];
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = sortedArray[index];
+
+    if (item == value) {
+      return [index, numTries];
+    } else if (item < value) {
+      return this.binarySearch(array, value, sortedArray, index + 1, end, numTries + 1)
+    } else if (item > value) {
+      return this.binarySearch(array, value, sortedArray, start, index - 1, numTries + 1)
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const searchFunction = event.target.searchFunction.value;
+    const searchValue = event.target.searchValue.value;
+    let searchIndex;
+    let searchLength;
+
+    if (searchFunction === "linearSearch") {
+      searchIndex = this.linearSearch(numberArray, searchValue)
+      searchLength = searchIndex ? searchIndex : numberArray.length;
+    } else {
+      const result = this.binarySearch(numberArray, searchValue);
+      searchIndex = result[0];
+      searchLength = result[1];
+    }
+
+    let message
+    if (searchIndex === -1) {
+      message = `${searchValue} Not Found! It took ${searchLength} searches!`
+    } else {
+      message = `${searchValue} found at Index: ${searchIndex}. It took ${searchLength} searches to find!`
+    }
+
+    event.target.searchResult.value = message;
+  }
+
   render() {
     return (
       <main className="App">
-        <textarea/>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <h2>Number Search</h2>
+          <div className="App__form_group">
+            <label htmlFor="searchFunction">Function</label>
+            <select id="searchFunction" name="searchFunction" required>
+              <option value="">Choose Function</option>
+              <option value="linearSearch">Linear Search</option>
+              <option value="binarySearch">Binary Search</option>
+            </select>
+          </div>
+          <div className="App__form_group">
+            <label htmlFor="searchValue">Search Value</label>
+            <input type="number" id="searchValue" name="searchValue" placeholder="Example: 23" required />
+          </div>  
+          <button type="submit">Submit</button>
+          <output name="searchResult" />
+        </form>
       </main>
     )
   }
